96f2409ed7982baa621d1ad0c92c45f75c994f09
Burdinova <iburdinova@yandex.ru>
2025-04-22 22:59:35 +0300

    22.04 part3

--- src/components/Header/HeaderLogIn.jsx
+++ src/components/Header/HeaderLogIn.jsx
@@ -4,12 +4,14 @@ import ModalButton from "../Button/ModalButton.jsx";
 import Modal from "../Modal/Modal.jsx";
 import TabSwich from "../TabSwitch/TabSwith.jsx";
 import TextInput from "../InputFields/TextInput.jsx";
+import SubmitButton from "../Button/SubmitButton.jsx";
 
 export default function HeaderLogIn() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [email, setTeamName] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const [activeTab, setActiveTab] = useState("login");
   const tabs = [
@@ -19,32 +21,60 @@ export default function HeaderLogIn() {
 
   return (
     <div className="header">
-      <div className="header__item">
-        <ModalButton text="Вход" onClick={() => setIsModalOpen(true)} />
-        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          <TabSwich
-            tabs={tabs}
-            activeTab={activeTab}
-            onTabClick={setActiveTab}
-          />
+      {/* <div className="header__item"> */}
+      <ModalButton text="Вход" onClick={() => setIsModalOpen(true)} />
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <TabSwich tabs={tabs} activeTab={activeTab} onTabClick={setActiveTab} />
 
-          <TextInput
-            id="email"
-            label="Почта:"
-            value={email}
-            onChange={(e) => setTeamName(e.target.value)}
-            placeholder="Введите почту"
-          />
-          <TextInput
-            id="password"
-            label="Пароль:"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Введите пароль"
-          />
-        </Modal>
-      </div>
+        {activeTab === "login" ? (
+          <div className="header__i">
+            <TextInput
+              id="email"
+              label="Почта:"
+              value={email}
+              onChange={(e) => setTeamName(e.target.value)}
+              placeholder="Введите почту"
+            />
+            <TextInput
+              id="password"
+              label="Пароль:"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Введите пароль"
+            />
+            <SubmitButton text="Войти" />
+          </div>
+        ) : (
+          <div className="header__i">
+            <TextInput
+              id="email"
+              label="Почта:"
+              value={email}
+              onChange={(e) => setTeamName(e.target.value)}
+              placeholder="Введите почту"
+            />
+            <TextInput
+              id="password"
+              label="Пароль:"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Введите пароль"
+            />
+            <TextInput
+              id="confirm-password"
+              label="Подтверждение пароля:"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder="Повторите пароль"
+            />
+            <SubmitButton text="Зарегистрироваться" />
+          </div>
+        )}
+      </Modal>
+      {/* </div> */}
     </div>
   );
 }
